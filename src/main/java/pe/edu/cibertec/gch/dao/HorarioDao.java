947692cf428492e55e040031665c99a61d77ff66
André Bedregal <andre.bedregal@gmail.com>
2013-05-28 21:01:44 -0500

    Agregar búsqueda por horario.

--- src/main/java/pe/edu/cibertec/gch/dao/HorarioDao.java
+++ src/main/java/pe/edu/cibertec/gch/dao/HorarioDao.java
@@ -1,12 +1,17 @@
 package pe.edu.cibertec.gch.dao;
 
+import java.util.List;
 import pe.edu.cibertec.gch.logica.GestorBase;
 import pe.edu.cibertec.gch.modelo.Horario;
+import pe.edu.cibertec.gch.modelo.TipoBusqueda;
 
 /**
  *
  * @author Student
  */
 public interface HorarioDao extends GestorBase<Horario> {
+    
+    void borrarTodos();
+    List<Horario> obtenerSegun(String descripcion, TipoBusqueda tipoBusqueda);
     
 }
