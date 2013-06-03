bf8e7e1db37b94d75863b3fd731954b4f7fe9fd7
DanJoas <ddcruzg@gmail.com>
2013-06-03 10:07:45 -0500

    interface e implemetacion dao para programas.

---
+++ src/main/java/pe/edu/cibertec/gch/dao/ProgramaDao.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.gch.dao;
+
+import java.util.List;
+import pe.edu.cibertec.gch.logica.GestorBase;
+import pe.edu.cibertec.gch.modelo.Programa;
+import pe.edu.cibertec.gch.modelo.TipoBusqueda;
+
+/**
+ *
+ * @author grupoPrograma
+ */
+public interface ProgramaDao extends GestorBase<Programa> {
+
+    public List<Programa> listarSegun(String titulo, String descripcion, TipoBusqueda tipoBusquedaEnum);
+}
