5af8b3e5306641840f1b1c21ff23cdcf3fd7873d
DanJoas <ddcruzg@gmail.com>
2013-06-03 10:07:17 -0500

    ordenando servlets accediendo al factory dao

---
+++ src/main/java/pe/edu/cibertec/gch/web/servlets/programa/BuscarProgramaServlet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.cibertec.gch.web.servlets.programa;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pe.edu.cibertec.gch.dao.FactoryDao;
+import pe.edu.cibertec.gch.dao.ProgramaDao;
+import pe.edu.cibertec.gch.modelo.Programa;
+import pe.edu.cibertec.gch.modelo.TipoBusqueda;
+import pe.edu.cibertec.gch.web.servlets.GchServletUtils;
+
+/**
+ *
+ * @author JAVA_MJ
+ */
+@WebServlet(name = "BuscarProgramasServlet", urlPatterns = {"/buscarProgramas"})
+public class BuscarProgramaServlet extends HttpServlet {
+    
+    private ProgramaDao programaDao = FactoryDao.getInstance().getProgramaDao();
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String titulo = req.getParameter("titulo"),
+                descripcion = req.getParameter("descripcion"),
+                tipoBusqueda = req.getParameter("tipoBusqueda");
+        
+        //Segun el codigo de tipo de busqueda obtener el valor del enum.
+        TipoBusqueda tipoBusquedaEnum = TipoBusqueda.obtenerPorCodigo(Integer.parseInt(tipoBusqueda));
+        
+        List<Programa> programas = programaDao.listarSegun(titulo, descripcion, tipoBusquedaEnum);
+        req.setAttribute("programa", programas);
+        // pinta los datos en el listado
+        GchServletUtils.reenviarAModulo("programa", req, resp);
+    }
+    
+}
---
+++ src/main/java/pe/edu/cibertec/gch/web/servlets/programa/RegistroProgramaServlet.java
@@ -0,0 +1,58 @@
+package pe.edu.cibertec.gch.web.servlets.programa;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pe.edu.cibertec.gch.dao.FactoryDao;
+import pe.edu.cibertec.gch.dao.ProgramaDao;
+import pe.edu.cibertec.gch.modelo.EstadoActividad;
+import pe.edu.cibertec.gch.modelo.Moneda;
+import pe.edu.cibertec.gch.modelo.Programa;
+
+/**
+ * Servlet para registrar un nuevo programa.
+ */
+@WebServlet(name = "RegistroProgramaServlet", urlPatterns = {"/registrarPrograma"})
+public class RegistroProgramaServlet extends HttpServlet {
+
+    private ProgramaDao programaDao = FactoryDao.getInstance().getProgramaDao();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        final String moneda = req.getParameter("moneda");
+
+        Programa nuevoPrograma = new Programa();
+        nuevoPrograma.setCodigo(req.getParameter("codigo"));
+        nuevoPrograma.setTitulo(req.getParameter("titulo"));
+        nuevoPrograma.setDescripcion(req.getParameter("descripcion"));
+        nuevoPrograma.setObjetivos(req.getParameter("objetivos"));
+        nuevoPrograma.setRequisitos(req.getParameter("requisitos"));
+        nuevoPrograma.setMoneda(moneda.equals("NS") ? Moneda.NuevosSoles : Moneda.DolaresUS);
+        nuevoPrograma.setPrecio(Double.parseDouble(req.getParameter("precio")));
+        nuevoPrograma.setEstado(EstadoActividad.Valido);
+        nuevoPrograma.setFechaInicial(fechaToDate(req.getParameter("fecha")));
+        nuevoPrograma.setDuracion(Integer.parseInt(req.getParameter("duracion")));
+
+        programaDao.registrar(nuevoPrograma);
+        resp.sendRedirect("listarProgramas");
+    }
+
+    private Date fechaToDate(String fecha) {
+
+        int dia = Integer.parseInt(fecha.substring(8)),
+                mes = Integer.parseInt(fecha.substring(5, 7)),
+                ano = Integer.parseInt(fecha.substring(0, 4));
+        final Calendar cal = Calendar.getInstance();
+        cal.setLenient(false);
+        cal.clear();
+        cal.set(ano, mes - 1, dia);
+
+        return cal.getTime();
+    }
+}
