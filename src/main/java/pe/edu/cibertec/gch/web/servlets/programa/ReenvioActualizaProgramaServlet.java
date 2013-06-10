39b8941b691bebef23ffb70630903cc9a041b361
PilarNicho <alpi136@gmail.com>
2013-06-09 22:59:42 -0500

    Modifica

--- src/main/java/pe/edu/cibertec/gch/web/servlets/programa/ReenvioActualizaProgramaServlet.java
+++ src/main/java/pe/edu/cibertec/gch/web/servlets/programa/ReenvioActualizaProgramaServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pe.edu.cibertec.gch.dao.FactoryDao;
+import pe.edu.cibertec.gch.dao.ProgramaDao;
+import pe.edu.cibertec.gch.modelo.Programa;
 import pe.edu.cibertec.gch.web.servlets.GchServletUtils;
 
 /**
@@ -13,9 +16,16 @@ import pe.edu.cibertec.gch.web.servlets.GchServletUtils;
  */
 @WebServlet(name = "ReenvioActualizaPrograma", urlPatterns = {"/irActualizaPrograma"})
 public class ReenvioActualizaProgramaServlet extends HttpServlet {
+     private ProgramaDao programaDao = FactoryDao.getInstance().getProgramaDao();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String codigo = req.getParameter("codigo");
+        
+        Programa progr = programaDao.consultarPorCodigo(codigo);
+        req.setAttribute("programa", progr);
+        
         GchServletUtils.reenviarAModificar("programa", req, resp);
     }
 }
