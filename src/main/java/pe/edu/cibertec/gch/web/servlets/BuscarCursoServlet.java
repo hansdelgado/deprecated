fd8f3fb56e999ba03c5847b1ce7e9dc90e0e3652
martin navarro <mnavarro2477@gmail.com>
2013-06-11 20:01:37 -0500

    Package filter Curso

--- src/main/java/pe/edu/cibertec/gch/web/servlets/BuscarCursoServlet.java
+++ src/main/java/pe/edu/cibertec/gch/web/servlets/BuscarCursoServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pe.edu.cibertec.gch.dao.CursoDao;
+import pe.edu.cibertec.gch.dao.FactoryDao;
 import pe.edu.cibertec.gch.logica.GestorCurso;
 import pe.edu.cibertec.gch.modelo.Curso;
 import pe.edu.cibertec.gch.modelo.TipoBusqueda;
@@ -23,6 +25,7 @@ import pe.edu.cibertec.gch.modelo.TipoBusqueda;
 public class BuscarCursoServlet extends HttpServlet {
     
     private GestorCurso gestorCurso = new GestorCurso();
+    private CursoDao cursoDao = FactoryDao.getInstance().getCursoDao();
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -39,7 +42,9 @@ public class BuscarCursoServlet extends HttpServlet {
         //Segun el codigo de tipo de busqueda obtener el valor del enum.
         TipoBusqueda tipoBusquedaEnum = TipoBusqueda.obtenerPorCodigo(Integer.parseInt(tipoBusqueda));
         
-        List<Curso> cursos = gestorCurso.listarSegun(codigo,nombre,descripcion,objetivos ,requisitos ,estado , duracion, tipoBusquedaEnum);
+        List<Curso> cursos = cursoDao.listarSegun(codigo,nombre,descripcion,objetivos ,requisitos ,estado , duracion, tipoBusquedaEnum);
+        //cursoDao.listarSegun(nombre, descripcion, objetivos, requisitos, estado, duracion, tipoBusquedaEnum)
+       // cursoDao.listarSegun(nombre, descripcion, tipoBusquedaEnum)
         
         req.setAttribute("cursos", cursos);
         // pinta los datos en el listado
